9c1cceaef4e0cf87da5662b5a96037b781bab908
adel <mohnagato@gmail.com>
2020-08-29 08:57:31 +0100

    refs change images to public folder

--- src/Images/Images.js
+++ src/Images/Images.js
@@ -1,59 +1,59 @@
-export const logoBea = require('./src/bea_logo.png')
-export const logoBuileo = require('./src/buileo.png')
-export const logoEp = require('./src/ep_logo.png')
-export const logoKhedemni = require('./src/khedemi.webp')
-export const logoUalger = require('./src/ualger1.png')
-export const user= require('./src/v5.png')
+export const logoBea = `${process.env.PUBLIC_URL}/src/bea_logo.png`
+export const logoBuileo = `${process.env.PUBLIC_URL}/src/buileo.png`
+export const logoEp = `${process.env.PUBLIC_URL}/src/ep_logo.png`
+export const logoKhedemni = `${process.env.PUBLIC_URL}/src/khedemi.webp`
+export const logoUalger = `${process.env.PUBLIC_URL}/src/ualger1.png`
+export const user= `${process.env.PUBLIC_URL}/src/v5.png`
 
-export const prof= require('./src/prof.png')
-export const react_logo= require('./src/react_logo.png')
-export const react= require('./src/react.svg')
-export const html_logo=require('./src/html-logo.png')
-export const wave=require('./src/wave.png')
+export const prof= `${process.env.PUBLIC_URL}/src/prof.png`
+export const react_logo= `${process.env.PUBLIC_URL}/src/react_logo.png`
+export const react= `${process.env.PUBLIC_URL}/src/react.svg`
+export const html_logo=`${process.env.PUBLIC_URL}/src/html-logo.png`
+export const wave=`${process.env.PUBLIC_URL}/src/wave.png`
 
-export const rn_logo=require('./src/rn_logo.png')
+export const rn_logo=`${process.env.PUBLIC_URL}/src/rn_logo.png`
 
-export const redux_logo=require('./src/redux.png')
-export const css_logo=require('./src/css_logo.png')
-export const js_logo=require('./src/js_logo.png')
-export const bootstrap_logo=require('./src/bootstrap_logo.png')
-export const bootstrap=require('./src/bootstrap.png')
-export const jquery_logo=require('./src/jquery_logo.png')
-export const jquery=require('./src/jquery.png')
-
-
-export const symfony_logo=require('./src/symfony_logo.png')
-export const symfony=require('./src/symfony.svg')
-
-export const php_logo=require('./src/php_logo.png')
-export const php=require('./src/php.png')
-
-export const codeigniter_logo=require('./src/codeigniter_logo.png')
-export const api_logo=require('./src/api_logo.png')
+export const redux_logo=`${process.env.PUBLIC_URL}/src/redux.png`
+export const css_logo=`${process.env.PUBLIC_URL}/src/css_logo.png`
+export const js_logo=`${process.env.PUBLIC_URL}/src/js_logo.png`
+export const bootstrap_logo=`${process.env.PUBLIC_URL}/src/bootstrap_logo.png`
+export const bootstrap=`${process.env.PUBLIC_URL}/src/bootstrap.png`
+export const jquery_logo=`${process.env.PUBLIC_URL}/src/jquery_logo.png`
+export const jquery=`${process.env.PUBLIC_URL}/src/jquery.png`
 
 
-export const docker_logo=require('./src/docker_logo.png')
-export const docker=require('./src/docker.png')
+export const symfony_logo=`${process.env.PUBLIC_URL}/src/symfony_logo.png`
+export const symfony=`${process.env.PUBLIC_URL}/src/symfony.svg`
 
-export const webpack_logo=require('./src/webpack_logo.png')
-export const webpack=require('./src/webpack.svg')
-export const mysql_logo=require('./src/mysql_logo.png')
-export const arch_logo=require('./src/arch_logo.png')
-export const git_logo=require('./src/git_logo.png')
-export const git=require('./src/git.png')
-export const vue=require('./src/vue.png')
-export const firebase= require('./src/firebase.png')
-export const node= require('./src/node.png')
-export const artelyes= require('./src/artelyes.png')
-export const material= require('./src/material.png')
-export const jquery_logo2 = require('./src/jquery-logo-2.png')
-export const express = require('./src/express.png')
+export const php_logo=`${process.env.PUBLIC_URL}/src/php_logo.png`
+export const php=`${process.env.PUBLIC_URL}/src/php.png`
 
-export const manga = require('./src/manga.jpg')
-export const js_dev = require('./src/js-dev.png')
-export const dev_skill = require('./src/dev-skill.png')
-export const contact = require('./src/contact.png')
-export const projects = require('./src/projects.png')
-export const amt2 = require('./src/amt2.png')
-export const freePik = require('./src/freepik.svg')
-export const bachlor = require('./src/bachlor.jpeg')
+export const codeigniter_logo=`${process.env.PUBLIC_URL}/src/codeigniter_logo.png`
+export const api_logo=`${process.env.PUBLIC_URL}/src/api_logo.png`
+
+
+export const docker_logo=`${process.env.PUBLIC_URL}/src/docker_logo.png`
+export const docker=`${process.env.PUBLIC_URL}/src/docker.png`
+
+export const webpack_logo=`${process.env.PUBLIC_URL}/src/webpack_logo.png`
+export const webpack=`${process.env.PUBLIC_URL}/src/webpack.svg`
+export const mysql_logo=`${process.env.PUBLIC_URL}/src/mysql_logo.png`
+export const arch_logo=`${process.env.PUBLIC_URL}/src/arch_logo.png`
+export const git_logo=`${process.env.PUBLIC_URL}/src/git_logo.png`
+export const git=`${process.env.PUBLIC_URL}/src/git.png`
+export const vue=`${process.env.PUBLIC_URL}/src/vue.png`
+export const firebase= `${process.env.PUBLIC_URL}/src/firebase.png`
+export const node= `${process.env.PUBLIC_URL}/src/node.png`
+export const artelyes= `${process.env.PUBLIC_URL}/src/artelyes.png`
+export const material= `${process.env.PUBLIC_URL}/src/material.png`
+export const jquery_logo2 = `${process.env.PUBLIC_URL}/src/jquery-logo-2.png`
+export const express = `${process.env.PUBLIC_URL}/src/express.png`
+
+export const manga = `${process.env.PUBLIC_URL}/src/manga.jpg`
+export const js_dev = `${process.env.PUBLIC_URL}/src/js-dev.png`
+export const dev_skill = `${process.env.PUBLIC_URL}/src/dev-skill.png`
+export const contact = `${process.env.PUBLIC_URL}/src/contact.png`
+export const projects = `${process.env.PUBLIC_URL}/src/projects.png`
+export const amt2 = `${process.env.PUBLIC_URL}/src/amt2.png`
+export const freePik = `${process.env.PUBLIC_URL}/src/freepik.svg`
+export const bachlor = `${process.env.PUBLIC_URL}/src/bachlor.jpeg`
