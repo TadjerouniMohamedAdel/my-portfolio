eb60dc5d1a0759722bebfc44284e2e7600f7321b
adel <mohnagato@gmail.com>
2020-09-07 19:53:29 +0100

    refs responsivite responsie resume

--- src/components/Timeline/Timeline.js
+++ src/components/Timeline/Timeline.js
@@ -9,11 +9,12 @@ const experiences = [
   {
       startDate:"Novembre 2018",
       endDate:"En cours",
-      jobName:"Développeur Web/Mobile Full Stack",
+      jobName:"Développeur Full Stack Web/Mobile",
       jobDescription:"Assurer le développement du site e-recrutement (l’ajout de nouvelles fonctionnalités ,correction des bugs https://www.emploipartner.com/fr/. ,Développement des applications interne de l’entreprise emploipartner (l’ajout de nouvelles fonctionnalités ,mise a jour des noyaux et la correction des bugs)",
       img:logoEp,
       imgWidth:80,
       imgHeight:80,
+      className:"img80x80",
       tech:[{name:"Symfony",logo:symfony},{name:"PHP",logo:php},{name:"Api Platform",logo:api_logo},{name:"JavaScript",logo:js_logo},{name:"HTML",logo:html_logo},{name:"CSS",logo:css_logo},{name:"JQuery",logo:jquery},{name:"Bootstrap",logo:bootstrap},{name:"React",logo:react},{name:"React Native",logo:react},{name:"Docker",logo:docker},{name:"Webpack",logo:webpack},{name:"git",logo:git},{name:"Mysql",logo:mysql_logo}]
   },
   {
@@ -23,6 +24,7 @@ const experiences = [
       jobDescription:"Développement d’une application web pour la gestion des œuvres sociales (prestations,primes,pres,Dons) des employees  la banque d’extérieur d ‘Algérie .",
       img:logoBea,
       imgWidth:90,
+      className:"img90x70",
       imgHeight:70,
       tech:[{name:"Codeigniter",logo:codeigniter_logo},{name:"PHP",logo:php},{name:"JavaScript",logo:js_logo},{name:"HTML",logo:html_logo},{name:"CSS",logo:css_logo},{name:"JQuery",logo:jquery},{name:"Bootstrap",logo:bootstrap},{name:"Mysql",logo:mysql_logo}]
 
@@ -46,7 +48,7 @@ export default class Timeline extends Component {
                       <div className="timeline-content ">
                           <h3>{item.jobName}</h3>
                           <p>
-                            <img src={item.img} width={item.imgWidth} height={item.imgHeight}  style={{float:"left",marginRight:20}}/>
+                            <img src={item.img} className={item.className}   style={{float:"left",marginRight:20}}/>
                             {item.jobDescription}
                           </p>
                           <div style={{marginTop:10}}>
