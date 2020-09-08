14326b371170b7b2ec5eb71f5104b62a9474463a
adel <mohnagato@gmail.com>
2020-09-08 17:19:49 +0100

    fixed speed

--- src/components/ProfileCard/SocialMenu.js
+++ src/components/ProfileCard/SocialMenu.js
@@ -3,11 +3,11 @@ import  './SocialMenu.css'
 export default function SocialMenu() {
     return (
     <ul className="list-social1">
-        <li className="li-github"><a className="social-link1" href="#"><i className="fab fa-github"></i></a></li>
-        <li className="li-linkedin"><a className="social-link1" href="#"><i className="fab fa-linkedin"></i></a></li>
-        <li className="li-skype"><a className="social-link1" href="#"><i className="fab fa-skype"></i></a></li>
-        <li className="li-google"><a className="social-link1" href="#"><i className="fab fa-google"></i></a></li>
-        <li className="li-facebook"><a className="social-link1" href="#"><i className="fab fa-facebook"></i></a></li>
+        <li className="li-github"><a target="_blank" className="social-link1" href="https://github.com/TadjerouniMohamedAdel"><i className="fab fa-github"></i></a></li>
+        <li className="li-linkedin"><a target="_blank" className="social-link1" href="https://www.linkedin.com/in/adel-mohamed-tadjerouni-147546164/"><i className="fab fa-linkedin"></i></a></li>
+        <li className="li-skype"><a target="_blank" className="social-link1" href="#"><i className="fab fa-skype"></i></a></li>
+        <li className="li-google"><a target="_blank" className="social-link1" href="#"><i className="fab fa-google"></i></a></li>
+        <li className="li-facebook"><a target="_blank" className="social-link1" href="https://web.facebook.com/adelnagato.tadj"><i className="fab fa-facebook"></i></a></li>
     </ul>
     )
 }
