4f8d917067177250c7f77ae0e07e28a896011742
adel <mohnagato@gmail.com>
2020-10-22 17:53:49 +0100

    fixed text

--- src/components/Biography/Biography.js
+++ src/components/Biography/Biography.js
@@ -16,7 +16,7 @@ export default function Biography() {
                     Je suis un développeur full stack passionné principalement par le web mais aussi pour le développement mobile desktop et le Devops en troisième degré .
                     je suis du type autodidacte, j'adore l'autoapprentissage et j'appuie sur cette faculté pour améliorer mes compétences techniques.
                     <br/>
-                    Ma grande motivation est d'être un full stack qui metrise très bien le FRONT et BACK END en même temps .
+                    Ma grande motivation est d'être un full stack qui maîtrise très bien le FRONT et BACK END en même temps .
                     même si c'est difficile de se concentrer sur les deux, je travaille et je me forme tous les jours pour atteindre cet objectif
                     </p>
                 </div>
