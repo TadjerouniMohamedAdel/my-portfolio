dbf3ab165deb70104c9e68f085b378dd10d7f83d
adel <mohnagato@gmail.com>
2021-04-17 16:29:10 +0100

    fixed target

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
-// const { i18n } = require('./next-i18next.config')
+const { i18n } = require('./next-i18next.config')
 
 module.exports = {
-  target: "serverless",
-  i18n: {
-    defaultLocale: 'en',
-    locales: ['en', 'fr'],
-    localeDetection: false,
-  },
+  i18n,
+  target: "experimental-serverless-trace",
 }
