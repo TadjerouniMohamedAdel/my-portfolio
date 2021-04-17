9d5dc1ef379465f0f483f2242e5e4a971654345d
adel <mohnagato@gmail.com>
2021-04-17 16:18:20 +0100

    try centralize config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
-const { i18n } = require('./next-i18next.config')
+// const { i18n } = require('./next-i18next.config')
 
 module.exports = {
   target: "serverless",
-  i18n,
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en', 'fr'],
+    localeDetection: false,
+  },
 }
